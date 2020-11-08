112392d2da22d095500de28a016584b26031974f
MICHAEL_TIEN <vantien3010892@gmail.com>
2020-11-08 16:19:19 +0700

    quan_li_mon_hoc

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,5 +1,8 @@
 var LopHoc = function(){
     this.danhSachSinhVien = [];
+    // use A
+    this.moTa = "Đây là lớp font-end 54 đang học git !!!"
+    // use B
     this.layDanhSachSinhVien = function(){
         return this.danhSachSinhVien;
     }
---
+++ models/MonHoc.js
@@ -0,0 +1,8 @@
+var MonHoc = function () {
+    this.maMonHoc = "";
+    this.tenMonHoc = "";
+    this.xuatThongTinMonHoc = function () {
+        console.log(`mã môn học: ${this.maMonHoc}`);
+        console.log(`tên môn học: ${this.tenMonHoc}`);
+    }
+}
